b64ca92d5ca65fe03330a534acce1eebd24a8c26
전상수 <jeonsangsu@jeonsangsuui-MacBookPro.local>
2023-03-13 16:51:21 +0900

    change ip address

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def my_url_for(request: Request, name: str, **path_params: any) -> str:
     url = request.url_for(name, **path_params)
     print(url)
     parsed = list(urllib.parse.urlparse(url))
-    parsed[1] = '52.79.233.189' 
+    parsed[1] = '52.79.233.189:80' 
     # parsed[1] = '127.0.0.1:8000'  # Change the domain name    
     # parsed[1] = 'greenoceansdata.kr'  # Change the domain name
     return urllib.parse.urlunparse(parsed)
